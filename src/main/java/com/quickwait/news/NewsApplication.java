2489f86af7871e338829a55822a8e7c6347ce40c
Daniel Dutra <danieldhsd@gmail.com>
2022-03-01 15:53:37 -0300

    Configurinf project to eureka client

--- src/main/java/com/quickwait/news/NewsApplication.java
+++ src/main/java/com/quickwait/news/NewsApplication.java
@@ -2,11 +2,21 @@ package com.quickwait.news;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@EnableEurekaClient
+@RefreshScope
+@OpenAPIDefinition(info =
+	@Info(title = "News API", version = "1.0", description = "Documentation News API v1.0")
+)
 public class NewsApplication {
 
 	public static void main(String[] args) {
